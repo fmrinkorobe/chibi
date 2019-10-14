614abba301258fbff8af418c537fd600a75a4b8b
FURUKAWA Maho <21716084@jwu.ac.jp>
2019-10-14 15:58:14 +0900

    Add

--- exp.py
+++ exp.py
@@ -12,3 +12,20 @@ class Val(object):
 v = Val(1)
 print(v)
 assert v.eval() == 1
+
+class Add(object):
+    __slots__=['left', 'right']
+    def __init__(self, a, b ):
+        self.left = a
+        self.right = b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+e = Add(1, 2)
+print(e.eval())
+assert e.eval() == 3
+
+e = Add(Val(1), Add(2,3))
+print(e.eval())
+
+print()
