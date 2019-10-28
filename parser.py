a97b9de2b78582e53326e73e387b148aebce35d0
FURUKAWA Maho <21716084@jwu.ac.jp>
2019-10-28 16:04:56 +0900

    output binary Add

--- parser.py
+++ parser.py
@@ -3,13 +3,14 @@ from exp import Val, Add
 def parse(s: str):
     num = int(s)
     return Val(num)
- e = parse("123")
- print(e)
 
- s = "123+456"
- pos = s.find('+')
- print('pos', pos)
+e = parse("123")
+print(e)
 
- s1 = s[Θ:pos]
- s2 = s[pos+1:]
- print(s, s1, s2)
+s = "123+456"
+pos = s.find('+')
+print('pos', pos)
+
+s1 = s[0:pos]
+s2 = s[pos+1:]
+print(s, s1, s2)
