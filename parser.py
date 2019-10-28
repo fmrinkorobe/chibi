8f28c5d2e7bd94d40e94a24617324ce247a61418
FURUKAWA Maho <21716084@jwu.ac.jp>
2019-10-28 16:52:15 +0900

    output Expr

--- parser.py
+++ parser.py
@@ -22,15 +22,24 @@ def parse(s: str):
         pos = s.find('*')
         s1 = s[0:pos]
         s2 = s[pos+1:]
+        return Add(parse(s1),parse(s2))
+    if s.find('/') > 0:
+        pos = s.find('/')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
         return Mul(parse(s1), parse(s2))
     if s.find('+') > 0:
         pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1),parse(s2))
+    if s.find('-') > 0:
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1),parse(s2))
     return Val(int(s))
 
-e = parse("1*2+3")
+e = parse("1-2-3")
 print(e, e.eval())
-e = parse("1+2*3")
-print(e, e.eval())
+assert e, e.eval() == -4
